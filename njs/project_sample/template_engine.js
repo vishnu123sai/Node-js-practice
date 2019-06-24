980ce01f7ade93bba421927027f461d216474b3a
vishnu123sai <vishnu123sai@gmail.com>
2019-06-25 04:49:28 +0530

    mode js practices files added

--- njs/project_sample/template_engine.js
+++ njs/project_sample/template_engine.js
@@ -3,6 +3,7 @@ var http  = require("http")
 var express  = require("express")  //  please install this before use using npm install express
 
 var app = express(); // express module is very easy and convienient to route the request ti the web pages
+app.use("/static", express.static("static"))  // this  is the we can use the static files in our application 
 
 // after that set our app to template engine  EJS
 app.set("view engine", "ejs")  // this step is mandatory 
